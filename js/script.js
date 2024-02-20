bb5d911c3475088f81574e642b4d2b2bcd26269e
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-20 16:06:14 +0000

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-var keylist = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
-    var temp = ''
-    function generatepass(plength){
-        temp = ''
-        for (i = 0; i < plength; i++){
-            temp += keylist.charAt(Math.floor(Math.random()*keylist.length))
-        }
-        return temp
-    }
-    function populateform(enterlength){
-        document.passGen.output.value = generatepass(enterlength)
-    }
+const passwordBox = document.getElementById("password");
+const length = 12;
 
-document.getElementById("test").innerHTML = populateform();
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+~|{}[]<>/-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    console.log(password);
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    console.log(password);
+    password += number[Math.floor(Math.random() * number.length)];
+    console.log(password);
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    console.log(password);
+    
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+        console.log(password)
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
